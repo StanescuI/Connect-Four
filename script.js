026fca3618eb806b401c34367efb29cce1a32f4c
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-04-02 13:46:41 +0300

    Game logic added
    Added game logic for rows, columns and diagonals,
    Added the result paragraph to keep track of the game state
    Added the winning state that stops players from adding tokens after one player wins or the game ends in a tie

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playerTurn = 1;
 let scoreBoard = [];
+let result = document.getElementById("result");
+let totalMoves = 0;
 for (let i = 0; i < 42; ++i) {
     scoreBoard[i] = 0;
 }
@@ -11,9 +13,15 @@ function markToken(columnIndex) {
             let buttonColor = document.getElementById(`${columnIndex + 7 * i}`);
             if (playerTurn === 1) {
                 buttonColor.style.backgroundColor = "red";
+                result.innerText = "Player 2 turn"
+                ++totalMoves;
+                checkWinner();
                 playerTurn = 2;
             } else {
                 buttonColor.style.backgroundColor = "blue";
+                result.innerText = "Player 1 turn"
+                ++totalMoves;
+                checkWinner();
                 playerTurn = 1;
             }
             break;
@@ -21,3 +29,51 @@ function markToken(columnIndex) {
     }
 }
 
+function checkWinner() {
+    for (let i = 0; i < 36;i += 7) {
+        for (let j = 0; j < 4; ++j) {
+            if (scoreBoard[j + i] !== 0 && scoreBoard[j + i] === scoreBoard[j + i + 1] &&
+            scoreBoard[j + i] === scoreBoard[j + i + 2] && scoreBoard[j + i] === scoreBoard[j + i + 3]) {
+                result.innerText = `Player ${playerTurn} won`;
+                gameOver();
+            }
+        }
+    }
+    for (let i = 0; i < 22; i += 7) {
+        for (let j = 0; j < 8; ++j) {
+            if (scoreBoard[j + i] !== 0 && scoreBoard[j + i] === scoreBoard[j + i + 7] &&
+            scoreBoard[j + i] === scoreBoard[j + i + 14] && scoreBoard[j + i] === scoreBoard[j + i + 21]) {
+                result.innerText = `Player ${playerTurn} won`;
+                gameOver();
+            }
+        }
+    }
+    for (let i = 0; i < 27;i += 7) {
+        for (let j = 0; j < 4; ++j) {
+            if (scoreBoard[j + i] !== 0 && scoreBoard[j + i] === scoreBoard[j + i + 8] &&
+            scoreBoard[j + i] === scoreBoard[j + i + 16] && scoreBoard[j + i] === scoreBoard[j + i + 24]) {
+                result.innerText = `Player ${playerTurn} won`;
+                gameOver();
+            }
+        }
+    }
+    for (let i = 0; i < 27;i += 7) {
+        for (let j = 0; j < 8; ++j) {
+            if (scoreBoard[j + i] !== 0 && scoreBoard[j + i] === scoreBoard[j + i + 6] &&
+            scoreBoard[j + i] === scoreBoard[j + i + 12] && scoreBoard[j + i] === scoreBoard[j + i + 18]) {
+                result.innerText = `Player ${playerTurn} won`;
+                gameOver();
+            }
+        }
+    }
+    if (totalMoves === 42) {
+        result.innerText = "The game ended in a tie !"
+        gameOver();
+    }
+}
+
+function gameOver() {
+    for (let i = 0; i < 42; ++i) {
+        scoreBoard[i] = 3;
+    }
+}
